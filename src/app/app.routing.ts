a9e1ebc297d907aef22d64472384f57f587adeb9
Justin Gomez <programjustin@gmail.com>
2017-07-21 08:33:23 -0700

    initialize project

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PlayerComponent } from './player/player.component';
 import { CharacterListComponent } from './character-list/character-list.component';
 import { AboutComponent } from './about/about.component';
+import { Scene01Component } from './scene01/scene01.component';
+import { Scene02Component } from './scene02/scene02.component';
+import { Scene03Component } from './scene03/scene03.component';
+import { Scene04Component } from './scene04/scene04.component';
+import { Scene05Component } from './scene05/scene05.component';
+import { Scene06Component } from './scene06/scene06.component';
 
 const appRoutes: Routes = [
   {
@@ -21,6 +27,34 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
+    path: 'scene01',
+    component: Scene01Component
+  },
+  {
+    path: 'scene02',
+    component: Scene01Component
+  },
+  {
+    path: 'scene03',
+    component: Scene01Component
+  },
+  {
+    path: 'scene04',
+    component: Scene01Component
+  },
+  {
+    path: 'scene05',
+    component: Scene01Component
+  },
+  {
+    path: 'scene06',
+    component: Scene01Component
   }
 ];
 
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -14,38 +14,84 @@ export class PlayerComponent implements OnInit {
   dexterity: number = 1;
   intelligence: number = 1;
   strength: number = 1;
-  checkFirst: boolean = true;
-  checkSecond: boolean = true;
-  checkThird: boolean = true;
-  checkFourth: boolean = true;
-
+  arraysone: any = [];
 
   constructor(private characterService: CharacterService) { }
 
-  addChar() {
-    this.charisma += 1;
-  }
+  ngOnInit() {}
 
-  addDex() {
-    this.dexterity += 1;
+  pushThis(string) {
+    let array = this.arraysone;
+    array.push(string);
   }
+  popLockItDropIt() {
+    let array = this.arraysone;
+    this.charisma = 1;
+    this.dexterity = 1;
+    this.intelligence = 1;
+    this.strength = 1;
 
-  addInt() {
-    this.intelligence += 1;
+    console.log(this.charisma);
+
+    for(var i = 0; i <= array.length + 2; i++){
+      array.pop();
+    }
   }
-
-  addStr() {
-    this.strength += 1;
-  }
-
-  ngOnInit() {
-  }
-
 
   submitForm(name: string, gender: string, charisma: number, dexterity: number, intelligence: number, strength: number) {
 
-    var newCharacter: Character = new Character(name, gender, charisma, dexterity, intelligence, strength);
+    var x = document.forms["addPerson"];
+    var i;
+    for (i = 0; i < x.length ;i++) {
+      //  console.log(x.elements[i].checked);
+       if((x.elements[i].checked === true) && (x.elements[i].value === 'char')) {
+        //  this.arrays.push();
+        let chars = 'char';
+        this.pushThis(chars);
+        // console.log(this.arraysone);
+      } else if ((x.elements[i].checked === true) && (x.elements[i].value === 'dex')) {
+        let dex = 'dex';
+        this.pushThis(dex);
+      } else if ((x.elements[i].checked === true) && (x.elements[i].value === 'intel')) {
+        let intel = 'intel';
+        this.pushThis(intel);
+      } else if ((x.elements[i].checked === true) && (x.elements[i].value === 'strength')) {
+        let strength = 'strength';
+        this.pushThis(strength);
+      }
+
+    }
+    for(var j = 0; j < this.arraysone.length; j++){
+
+      if(this.arraysone[j] === 'char') {
+
+        this.charisma += 1;
+        console.log(charisma);
+
+      } else if(this.arraysone[j] === 'dex') {
+
+        this.dexterity += 1;
+
+      } else if(this.arraysone[j] === 'intel') {
+
+        this.intelligence += 1;
+
+      } else if(this.arraysone[j] === 'strength') {
+
+        this.strength += 1;
+
+      }
+    }
+
+    var newCharacter: Character = new Character(name, gender, this.charisma, this.dexterity, this.intelligence, this.strength);
     this.characterService.addCharacter(newCharacter);
     console.log(newCharacter);
+
+    this.popLockItDropIt();
+    console.log(this.charisma);
+
+
+
   }
+
 }
