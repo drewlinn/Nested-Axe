5b44a78fd8c7ae20d73d14618131a363446320c2
Andrew <expandrew@gmail.com>
2017-07-24 09:14:51 -0700

    change paths from character add button, change our start button to a select this character and implement our character creation changes so that our stat generation works correctly.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,12 +6,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PlayerComponent } from './player/player.component';
 import { CharacterListComponent } from './character-list/character-list.component';
 import { AboutComponent } from './about/about.component';
-<<<<<<< HEAD
-=======
 import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
 //scene components
->>>>>>> 9a200c2c624c0030d4812c102cde619fb25ca7fb
 import { Scene01Component } from './scene01/scene01.component';
 import { Scene02Component } from './scene02/scene02.component';
 import { Scene03Component } from './scene03/scene03.component';
@@ -41,37 +38,15 @@ const appRoutes: Routes = [
     component: AboutComponent
   },
   {
-<<<<<<< HEAD
     path: 'welcome',
     component: WelcomeComponent
   },
   {
-=======
->>>>>>> 9a200c2c624c0030d4812c102cde619fb25ca7fb
     path: 'scene01',
     component: Scene01Component
   },
   {
     path: 'scene02',
-<<<<<<< HEAD
-    component: Scene01Component
-  },
-  {
-    path: 'scene03',
-    component: Scene01Component
-  },
-  {
-    path: 'scene04',
-    component: Scene01Component
-  },
-  {
-    path: 'scene05',
-    component: Scene01Component
-  },
-  {
-    path: 'scene06',
-    component: Scene01Component
-=======
     component: Scene02Component
   },
   {
@@ -85,7 +60,10 @@ const appRoutes: Routes = [
   {
     path: 'scene05',
     component: Scene05Component
->>>>>>> 9a200c2c624c0030d4812c102cde619fb25ca7fb
+  },
+  {
+    path: 'scene06',
+    component: Scene06Component
   }
 ];
 
