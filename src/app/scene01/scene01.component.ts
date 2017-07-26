9c3a6923aad9d7b6362c68b2cf37c340293b35f1
Justin Gomez <programjustin@gmail.com>
2017-07-26 09:14:01 -0700

    add business logic

--- src/app/scene01/scene01.component.ts
+++ src/app/scene01/scene01.component.ts
@@ -1,25 +1,27 @@
-import { Component,
-          OnInit,
-          trigger,
-          state,
-          style,
-          transition,
-          animate,
-          keyframes,
-          group
-        } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { CharacterDetailComponent } from '../character-detail/character-detail.component';
+
+import { Character } from '../character.model';
+import { CharacterService } from '../character.service';
 
 @Component({
   selector: 'app-scene01',
   templateUrl: './scene01.component.html',
   styleUrls: ['./scene01.component.scss'],
+  providers: [CharacterService]
 })
 export class Scene01Component implements OnInit {
-  // @Input() 
+  characters: FirebaseListObservable<any[]>;
+  @Input() selectedCharacter;
+  characterToPlay: Character;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private characterService: CharacterService) { }
 
   ngOnInit() {
+    this.characters = this.characterService.getCharacters();
   }
 
 }
